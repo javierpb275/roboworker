9cdaf3c36ac689dde5479da0ddef0f52b2963080
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-06-18 23:47:51 +0200

    installed tilt to give a cool styles to the product icons

--- client/src/components/product/product.component.jsx
+++ client/src/components/product/product.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import Tilt from 'react-tilt';
 
 const Product = ({id, name, icon, price}) => (
     <div>
-        <div>
-            <img alt="product-icon" src={icon}/>
-            <div>
+        <Tilt className="Tilt" options={{ max : 55 }} style={{ height: 250, width: 250 }}>
+            <div className="Tilt-inner pa3">
+                <img style={{paddingTop:'5px'}} alt="product-icon" src={icon}/>
                 <h6>{name}</h6>
                 <p>{price}</p>
             </div>
-        </div>
+        </Tilt>
     </div>
 );
 
