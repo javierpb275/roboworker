8daf5acd4b3f241bf7c0cb6b99e61e8c034d51df
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-06-20 00:03:40 +0200

    changed state. removed UserCard from render and added Card. Pass all the state directly to the card component. created onClickEarnCoins function which makes the user get goins when they click on the workButton

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 
 //COMPONENTS
 import Navigation from './components/navigation/navigation.component';
-import UserCard from './components/user-card/user-card.component';//This component displays the user image and user information. It's a parent of Card
+import Card from './components/card/card.component';
+//import UserCard from './components/user-card/user-card.component';//This component displays the user image and user information. It's a parent of Card
 import WorkButton from './components/work-button/work-button.component';//This component makes the user earn coins
 import ProductsList from './components/products-list/products-list.component';
 
@@ -20,32 +21,37 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      users: [],
+      id: users[0].id,
+      username: users[0].username, 
+      email: users[0].email,
+      coins: users[0].coins
     }
   }
 
 
   //This gets mounted everytime we run the page and it updates the state
   componentDidMount() {
-    this.setState({ users: users });
+    this.setState({ 
+      id: this.state.id,
+      username: this.state.username, 
+      email: this.state.email,
+      coins: this.state.coins
+     });
   }
 
   //This function makes the user coins amount increase. It is made for the WorkButton Component
-  onClickEarnCoins = (event) => {
-    //const {coins} = this.state.users.coins;
-    //this.setState({coins: coins + 1});
-   console.log("click")
-
+  onClickEarnCoins = () => {
+    this.setState({ coins: this.state.coins + 1 });
   };
 
 
   render() {
-    const {users} = this.state;
+    const {id, username, email, coins} = this.state;
   return (
     <div className="App">
       <Navigation/>
       <h1>ROBOWORKER</h1>
-      <UserCard users={users}/>
+      <Card id={id} username={username} email={email} coins={coins}/>
       <WorkButton earnCoins={this.onClickEarnCoins}/>
       <ProductsList products={products}/>
     </div>
--- client/src/components/user-card/user-card.component.jsx
+++ client/src/components/user-card/user-card.component.jsx
@@ -1,9 +1,9 @@
 //This UserCard component receives all the users and here we loop over those users in order to get all their properties and we pass them to the Card component.
-
+/*
 import React from 'react';
 import Card from '../card/card.component';
 
-const UserCard = ({users}) => {
+const UserCard = ({users, coins}) => {
     return (
     <div>
         {
@@ -14,7 +14,7 @@ const UserCard = ({users}) => {
                     id={users[i].id}
                     username={users[i].username}
                     email={users[i].email}
-                    coins={users[i].coins}
+                    coins={coins}
                     />
                 );
             })
@@ -24,3 +24,4 @@ const UserCard = ({users}) => {
 }
 
 export default UserCard;
+*/
