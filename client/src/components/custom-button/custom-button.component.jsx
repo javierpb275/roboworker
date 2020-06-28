aa25d9265590a294f5fe94824997538bb099d786
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-06-28 15:33:25 +0200

    added new custom button and passed img to custom button

--- client/src/components/custom-button/custom-button.component.jsx
+++ client/src/components/custom-button/custom-button.component.jsx
@@ -2,9 +2,12 @@
 
 import React from 'react';
 
-const WorkButton = ({handleClick}) => (
+const WorkButton = ({handleClick, icon, title}) => (
     <div>
-        <button onClick={() => handleClick()}>WORK</button>
+        <button onClick={() => handleClick()}>
+            <img src={icon}/>
+            <p>{title}</p>
+        </button>
     </div>
 );
 
