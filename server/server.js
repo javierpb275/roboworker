2f30c39f72340d8328a2bc7f60ab8ce1bae746f9
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-09 17:01:56 +0200

    created basic route to make sure everything is working.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res)=> {
+    res.send('this is working');
+})
+
+
 //check if our server is running:
 app.listen(3001, ()=> {
     console.log('app is running on port 3001');
