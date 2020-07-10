0d4f545795edfb386b9ad0e9d0276ed885c259c0
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-10 16:00:29 +0200

    created fake database in our server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,28 @@
 const express = require('express');
 const app = express();
 
+//fake database
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            coins: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'banana',
+            coins: 0,
+            joined: new Date()
+        }
+    ]
+}
+
 app.get('/', (req, res)=> {
     res.send('this is working');
 })
