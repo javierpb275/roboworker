296680d221ca643391f16979209614ee487f1d79
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-29 23:15:20 +0200

    created controllers folder and register.js and moved /register endpoint there

--- server/server.js
+++ server/server.js
@@ -30,6 +30,9 @@ const db = knex({
   });
 
 
+//CONTROLLERS
+const register = require('./controllers/register');
+
 
 app.get('/', (req, res) => {
     res.send(database.users);
@@ -69,41 +72,7 @@ app.post('/signin', (req, res) => {
 
 
 //REGISTER:
-app.post('/register', (req, res) => {
-
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
-            })
-    
-                .then(user => {
-                    res.json(user[0]);
-                })
-
-            })
-
-            .then(trx.commit)
-            .catch(trx.rollback)
-
-        })
-
-    .catch(err => res.status(400).json('unable to register'))
-    
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
 
 //PROFILE/:USERID (get the user for the homepage):
