ebdd5115e1c6a7aa10f030901585ba950bc02648
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-29 23:28:06 +0200

    created signin.js and moved /signin endpoint there

--- server/server.js
+++ server/server.js
@@ -32,6 +32,7 @@ const db = knex({
 
 //CONTROLLERS
 const register = require('./controllers/register');
+const signin = require('./controllers/signin');
 
 
 app.get('/', (req, res) => {
@@ -40,35 +41,8 @@ app.get('/', (req, res) => {
 
 
 //SIGN IN:
-app.post('/signin', (req, res) => {
-
-    db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-        .then(data => {
-
-           const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-
-           if (isValid) {
-
-               return db.select('*').from('users')
-               .where('email', '=', req.body.email)
-               .then(user => {
-                res.json(user[0])
-               })
-
-               .catch(err => res.status(400).json('unable to get user'));
-
-           } 
-           
-           else {
-            res.status(400).json('wrong credentials')
-           }
-
-        })
-
-        .catch(err => res.status(400).json('wrong credentials'))
-
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)}) 
+    
 
 
 //REGISTER:
