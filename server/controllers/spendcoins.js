f75b634df74a029940d2b3a7aac78aa26ccc9b95
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-30 17:25:26 +0200

    fixed issue in handleSpendCoins. now we are able to decrease the amount of coins  of the user when they purchase and it changes in the database. PENDING FIX ISSUE IN THE FRONTEND, THE NUMBER OF COINS DOES NOT CHANGE IN THE WEB.

--- server/controllers/spendcoins.js
+++ server/controllers/spendcoins.js
@@ -4,36 +4,35 @@ const handleSpendCoins = (req, res, db) =>  {
 
     const { id, price } = req.body;
 
-    const user = db('users').where('id', '=', id).then(user => {res.json(user[0])})
+    db('users').where('id', '=', id).then(user => {
+
+        if (typeof user !== "undefined") {
+
+            if (user[0].coins >= price) {
     
-    const coins = db('users').where('id', '=', id).then(user => {res.json(user[0].coins)})
-
-    if (typeof user !== "undefined") {
-
-        if (coins >= price) {
-
-            return db('users').where('id', '=', id)
-
-            .decrement('coins', price)
-
-            .returning('coins')
-
-            .then(coins => {
-                res.json(coins[0]);
-            })
-
-            .catch(err => res.status(400).json('unable to spend coins'));
+                return db('users').where('id', '=', id)
+    
+                .decrement('coins', price)
+    
+                .returning('coins')
+    
+                .then(coins => {
+                    res.json(coins[0]);
+                })
+    
+                .catch(err => res.status(400).json('unable to spend coins'));
+            }
+    
+            else {
+                return res.status(202).json('You can`t afford this product. Keep Working!');
+            }
         }
-
+    
         else {
-            return res.status(202).json('You can`t afford this product. Keep Working!');
+            res.status(404).json('user id not found');
         }
-    }
-
-    else {
-        res.status(404).json('user id not found');
-    }
-
+    })
+    
 }
 
 module.exports = {
